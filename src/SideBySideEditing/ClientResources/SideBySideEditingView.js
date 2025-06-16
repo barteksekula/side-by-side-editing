e2a6381271795875bad9f100989fd26cb0e0297a
Bartosz Sekula <Bartosz.Sekula@optimizely.com>
2025-06-16 20:05:25 +0200

    Make sure to select the correct tab
    Closes #2

--- src/SideBySideEditing/ClientResources/SideBySideEditingView.js
+++ src/SideBySideEditing/ClientResources/SideBySideEditingView.js
@@ -1,12 +1,52 @@
 define([
     "dojo/_base/declare",
+    "dojo/aspect",
     "dojo/topic",
     "epi-cms/contentediting/FormEditing"
 ], function (
     declare,
+    aspect,
     topic,
     FormEditing
 ) {
+
+    // We need to check what is the active tab and programatically click it
+    // This happens because at some point CMS added so called `sticky tab` which is
+    // only preserved in the Forms view, while now we have Side by Side view
+    function ensureCorrectTabSelected(form) {
+        var item = form.domNode.querySelector("div[data-dojo-attach-point='tablistWrapper']")
+
+        if(item && item.children && item.children[0]) {
+            var tablist = item.children[0];
+            var checkedIndex = -1;
+            if(tablist && tablist.children && tablist.children.length) {
+                for (var i = 0; i < tablist.children.length; i++) {
+                    if(tablist.children[i].classList.contains("dijitChecked")) {
+                        checkedIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            if(checkedIndex !== -1) {
+                var originalTab = tablist.children[checkedIndex];
+
+                var adjacentIndex;
+                if(checkedIndex < tablist.children.length - 1) {
+                    adjacentIndex = checkedIndex + 1;
+                } else if(checkedIndex > 0) {
+                    adjacentIndex = checkedIndex - 1;
+                } else {
+                    adjacentIndex = -1;
+                }
+
+                if(adjacentIndex !== -1) {
+                    tablist.children[adjacentIndex].click();
+                    originalTab.click();
+                }
+            }
+        }
+    }
 
     return declare([FormEditing], {
         // tags:
@@ -22,6 +62,15 @@ define([
             this.inherited(arguments);
             form.resize();
             topic.publish("resize-sidebyside");
+
+            var formCreateHandle = aspect.after(form, "onFormCreated", () => {
+                formCreateHandle.remove();
+                formCreateHandle = null;
+
+                if (form.domNode) {
+                    ensureCorrectTabSelected(form);
+                }
+            });
         }
     });
 });
